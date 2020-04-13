a4af62a3048f8fdcfc7a632e099701e042ce63be
Aaron Davida <adavi@microsoft.com>
2020-04-12 19:15:19 -0700

    use react-responsive carousel; accept url instead of id; styling tweaks;

---
+++ SongOfTheDay/SpotifySettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongOfTheDay
+{
+    public class SpotifySettings
+    {
+        public string SpotifyClientId { get; set; }
+        public string SpotifyClientSecret { get; set; }
+    }
+}
